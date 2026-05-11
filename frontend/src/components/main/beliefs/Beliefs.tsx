9085dcbaea592b1bbfe2208fe1b1320ff3763432
Robitza777 <robertcozma777@gmail.com>
2026-05-11 15:51:43 +0300

    Update Beliefs.tsx

--- frontend/src/components/main/beliefs/Beliefs.tsx
+++ frontend/src/components/main/beliefs/Beliefs.tsx
@@ -2,10 +2,10 @@ import FinalNavbar from "../utils/FinalNavbar";
 import BeliefContent from "./BeliefContent";
 
 export default function Beliefs() {
-    return (
-        <>
-            <FinalNavbar />
-            <BeliefContent />
-        </>
-    );
+  return (
+    <>
+      <FinalNavbar />
+      <BeliefContent />
+    </>
+  );
 }
