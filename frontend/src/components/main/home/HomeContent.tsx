855e5508ebf87e110954b0a2d6656ea54b3c9359
Robitza <robertcozma777@gmail.com>
2026-05-15 16:21:27 +0300

    Yellow Theme Final Video

--- frontend/src/components/main/home/HomeContent.tsx
+++ frontend/src/components/main/home/HomeContent.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from "../../../components/theme-provider";
-import videoYellow from "../../../assets/animatie_blender/24fpsYellow.mkv";
+import videoYellow from "../../../assets/animatie_blender/24fpsYellowCycles.webm";
 import videoPurple from "../../../assets/animatie_blender/24fpsPurpleCycles.mkv";
 import videoTurquoise from "../../../assets/animatie_blender/24fpsTurquoiseCycles.mkv";
 import { useState } from "react";
